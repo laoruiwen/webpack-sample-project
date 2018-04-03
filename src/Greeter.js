5198e94f98c4aedf74df7a1ccc0045ecc98982d1
laoruiwen <421365967@qq.com>
2018-04-03 14:46:33 +0800

    first commit

---
+++ src/Greeter.js
@@ -0,0 +1,9 @@
+/**
+ * Created by raven on 2018/3/27.
+ */
+// Greeter.js
+module.exports = function() {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greetings!";
+    return greet;
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by raven on 2018/3/27.
+ */
+//main.js
+const greeter = require('./Greeter.js');
+document.querySelector("#root").appendChild(greeter());
